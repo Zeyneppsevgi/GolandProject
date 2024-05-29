1d90f79656ae575093d9cf497e8cde1c12f2ef67
Zeyneppsevgi <zeyneppsevgi@gmail.com>
2024-05-29 11:52:11 +0300

    golangTest

---
+++ product-app/test/infrastructure/product_repository_test.go
@@ -0,0 +1,17 @@
+package infrastructure
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	t.Run("TestAdd", func(t *testing.T) {
+		actual := Add(10, 20)
+		assert.Equal(t, 30, actual)
+	})
+}
+
+func Add(x int, y int) int {
+	return x + y
+}
